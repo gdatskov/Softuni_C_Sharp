ffb24ab2db5915614aeb179d5d73e82c2fa7f7fa
gdatskov <103522803+gdatskov@users.noreply.github.com>
2022-11-23 14:13:24 +0200

    first commit

---
+++ C# Fundamentals/First Steps/Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Test World!");
+            Console.WriteLine("Hello Test!");
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello asd!");
+            
+        }
+    }
+}
